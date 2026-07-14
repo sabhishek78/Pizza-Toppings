951f44818861f2809fcbf24e14b2d9462ae152e2
Abhishek Shrivastava <abhishekshrivastava78@gmail.com>
2026-07-14 13:08:16 +0530

    add pizza functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,67 @@ import './App.css'
 const options = ['Bell Pepper', 'Sausage', 'Pepperoni', 'Pineapple'];
 
 //Static array of pizza options offered. 
-
 export default function App() {
   const [selected, setSelected] = useState([]);
-
-  const toggleTopping = ({target}) => {
-    const clickedTopping = target.value;
-    setSelected((prev) => {
-     // check if clicked topping is already selected
-      if (prev.includes(clickedTopping)) {
-        // filter the clicked topping out of state
-        return prev.filter(t => t !== clickedTopping);
-      } else {
-        // add the clicked topping to our state
-        return [clickedTopping, ...prev];
-      }
-    });
-  };
-
+  function addPizza(option) {
+    setSelected(prev => [...prev, option]);
+  }
+  const orderString = selected.join(", ");
+  const toppings = options.map((option) => <button onClick={() => addPizza(option)}>Add {option}</button>)
   return (
     <div>
-      {options.map(option => (
-        <button value={option} onClick={toggleTopping} key={option}>
-          {selected.includes(option) ? 'Remove ' : 'Add '}
-          {option}
-        </button>
-      ))}
-      <p>Order a {selected.join(', ')} pizza</p>
+      {toppings}
+      <p>Order {orderString} pizza</p>
     </div>
-  );
+  )
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function App() {
+//   const [selected, setSelected] = useState([]);
+
+//   const toggleTopping = ({target}) => {
+//     const clickedTopping = target.value;
+//     setSelected((prev) => {
+//      // check if clicked topping is already selected
+//       if (prev.includes(clickedTopping)) {
+//         // filter the clicked topping out of state
+//         return prev.filter(t => t !== clickedTopping);
+//       } else {
+//         // add the clicked topping to our state
+//         return [clickedTopping, ...prev];
+//       }
+//     });
+//   };
+
+//   return (
+//     <div>
+//       {options.map(option => (
+//         <button value={option} onClick={toggleTopping} key={option}>
+//           {selected.includes(option) ? 'Remove ' : 'Add '}
+//           {option}
+//         </button>
+//       ))}
+//       <p>Order a {selected.join(', ')} pizza</p>
+//     </div>
+//   );
+// }
 
 
